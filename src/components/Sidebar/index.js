e5e39d0565f723716cd1091a40f565c991ce0178
Adebanjo Adeniji <aeben.adebanjo@gmail.com>
2022-10-21 22:04:06 +0100

    initial commit after installing all dependencies and node packages

---
+++ src/components/Sidebar/index.js
@@ -0,0 +1,22 @@
+import './index.scss'
+import { Link, NavLink } from 'react-router-dom'
+import Logo from '../../assets/images/logo-s.png'
+import LogoSubtitle from '../../assets/images/logo_sub.png'
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faHome } from '@fortawesome/free-solid-svg-icons'
+
+const Sidebar = () => (
+  <div className="nav-bar">
+    <Link className="logo" to="/">
+      <img src={Logo} alt="logo" />
+      <img className="sub-logo" src={LogoSubtitle} alt="adebanjo" />
+    </Link>
+    <nav>
+      <NavLink exact="true" activeclassname="active" to="/">
+        <FontAwesomeIcon icon={faHome} color="#4d4d4e" />
+      </NavLink>
+    </nav>
+  </div>
+)
+
+export default Sidebar
